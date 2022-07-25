e781dc82b5f5f044131017e33b201a7cdb1fa513
Hassan Jawhar <hassanjawhar@protonmail.com>
2022-07-25 14:40:46 +0300

    Updated docker

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use actix_web::{App, HttpServer};
-mod routes;
-mod models;
+use actix_web::{web::scope, App, HttpServer};
 mod data;
+mod models;
+mod routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(crate::routes::hello)
-            .service(crate::routes::get_cars)
-            .service(crate::routes::post_cars)
+        App::new().service(
+            scope("/api")
+                .service(crate::routes::hello)
+                .service(crate::routes::get_cars)
+                .service(crate::routes::post_cars),
+        )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
